c4f41abb872428067736668d40ca4d3a7fb20cf5
Sikandar Hashmi <sikandar.salman@gmail.com>
2019-11-19 20:54:39 -0500

    added functionality

--- main.py
+++ main.py
@@ -7,20 +7,26 @@
 # longRepeat("abcdefghijklmnopqrstuvwxyz") => 1
 # longRepeat("") => 0
 
+import math
 def longRepeat(string):
     string_length = len(string)
     long_list = []
     longest = ""
 
-    for char in range(string_length - 1):
-        print(string[char])
-        if string[char] == string[char+1]:
-            longest = longest + string[char]
-            return longest
-        elif string[char] != string[char]:
-            long_list.append(len(longest))
-    return max(long_list)
-longRepeat("aaabbbbcccccdddd")
+    if string_length > 0:
+
+        for i in range(string_length-1):
+        
+            if string[i] == string[i + 1]:
+                longest = longest + string[i]
+            else:
+                long_list.append(len(longest) + 1)
+                longest = ""
+        return max(long_list)
+    else:
+        return string_length
 
 
-# Incomplete
+print(longRepeat("bdsagbgagggaaatttyyyyau"))
+
+
